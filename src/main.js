976fb44a9e1bf3e61e330824949f0c3b63545559
osuke-noguchi <noguchi.nodai@gmail.com>
2020-11-24 11:03:35 +0900

    stylelintの修正

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
-import VueCompositionAPI from '@vue/composition-api';
 
 Vue.use(VueCompositionAPI);
+
 Vue.config.productionTip = false;
 
 new Vue({
